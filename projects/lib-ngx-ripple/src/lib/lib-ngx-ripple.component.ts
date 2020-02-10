af178419e9a460ff289d04e5f8869901c40350a4
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-02-10 15:00:24 +0900

    :art: Remove duplicated 'lib-'

--- projects/lib-ngx-ripple/src/lib/lib-ngx-ripple.component.ts
+++ projects/lib-ngx-ripple/src/lib/lib-ngx-ripple.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'lib-lib-ngx-ripple',
+  selector: 'lib-ngx-ripple',
   template: `
     <p>
       lib-ngx-ripple works!
