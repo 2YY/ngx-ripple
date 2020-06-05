f71415fd4d804cbafb5523cf11cfd56f6bf06523
2YY <hkgeuobmaoakneotpuhtesnotekheo@gmail.com>
2020-06-05 23:15:29 +0900

    :rotating_light: Remove Code Climate alert

--- projects/ngx-ripple/karma.conf.js
+++ projects/ngx-ripple/karma.conf.js
@@ -1,20 +1,20 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const plugins = [
+  require('karma-jasmine'),
+  require('karma-chrome-launcher'),
+  require('karma-jasmine-html-reporter'),
+  require('karma-coverage-istanbul-reporter'),
+  require('@angular-devkit/build-angular/plugins/karma')
+];
+
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    plugins,
+    client: {clearContext: false},
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage/ngx-ripple'),
       reports: ['html', 'lcovonly', 'text-summary'],
@@ -26,12 +26,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+    customLaunchers: {ChromeHeadlessCI: {base: 'ChromeHeadless', flags: ['--no-sandbox']}},
     singleRun: false,
     restartOnFileChange: true
   });
